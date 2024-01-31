c6be7f3caae7f3d414065ea3b6a2eb59489dba2d
Yash Kadadi <yamu1001@gmail.com>
2024-01-31 22:45:37 +0000

    fixed Monte Carlo to break on collision. Need to move collision code into simulator step(). MonteCarlo.py will handle the output of the step()

--- validation/stresstests/MonteCarlo.py
+++ validation/stresstests/MonteCarlo.py
@@ -12,8 +12,8 @@ class MonteCarlo(object):
         self.n_simulations = n_simulations
         self.noise_mean = noise_mean
         self.noise_std = noise_std
-        # self.steps = steps
-        self.steps = 1
+        self.steps = steps
+        # self.steps = 1
         self.collision_grid = collision_grid
 
     # take in a state vector and output the grid coordinate
@@ -29,18 +29,18 @@ class MonteCarlo(object):
             self.simulator.reset()
             print(f"Starting simulation {i}")
             for j in trange(self.steps):
+                
                 noise = torch.normal(self.noise_mean, self.noise_std)
                 print(f"Step {j} with noise: {noise}")
                 self.simulator.step(noise)
-                
+                #TODO: move collision code to NerfSimulator.py 
                 current_state = self.simulator.current_state
                 current_state_gridCoord = self.stateToGridCoord(current_state)
                 collided = self.collision_grid[current_state_gridCoord]
+                self.collisions[noise] = collided
                 if collided:
                     print(f"Drone collided in state {current_state}")
+                    break
                 else:
                     print(f"Drone did NOT collide in state {current_state}")
-
-                self.collisions[noise] = collided
-            
 
