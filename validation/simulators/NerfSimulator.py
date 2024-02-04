a5ea67a046e9dbf7339ba0bf053fa3699fa4b702
Jacob Frausto <jacob_frausto@brown.edu>
2024-02-04 00:36:46 -0800

    delete reset_iters

--- validation/simulators/NerfSimulator.py
+++ validation/simulators/NerfSimulator.py
@@ -129,7 +129,6 @@ class NerfSimulator(gym.Env):
             observation (object): the initial observation.
         """
         self.clear_workspace()
-        self.dynamics.reset_iters()
         self.iter = 0
 
         # Reinitialize dynamics
