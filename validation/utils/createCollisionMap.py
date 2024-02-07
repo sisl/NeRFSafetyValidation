6b4a6bde60071394bfd8df36db1ac98e6378b445
Yash Kadadi <33234786+yamu1001@users.noreply.github.com>
2024-02-06 16:02:26 -0800

    utils for creating the collision map and SDF

---
+++ validation/utils/createCollisionMap.py
@@ -0,0 +1,69 @@
+import bmesh
+import bpy
+import numpy as np
+
+D = bpy.data
+C = bpy.context
+
+COLLISION_MESHES = [
+    D.objects["Mesh_0"],
+    D.objects["Mesh_1"],
+    D.objects["Mesh_2"],
+    D.objects["Mesh_3"],
+    D.objects["Mesh_4"],
+    D.objects["Mesh_5"],
+    D.objects["Mesh_6"],
+    D.objects["Mesh_7"],
+]
+
+GRANULARITY = 20 # how many points to sample per meter
+
+# define the range of the collision map in world coordinates. We got these from looking at the scene in Blender
+START_X = -1.4
+END_x = 1
+START_Y = -1.3
+END_Y = 1
+START_Z = -0.1
+END_Z = 0.5
+
+def worldToIndex(world, start, granularity):
+    return int(np.floor((world - start) * granularity))
+
+def indexToWorld(index, start, granularity):
+    return index / granularity + start
+
+X_RANGE = worldToIndex(END_x, START_X, GRANULARITY)
+Y_RANGE = worldToIndex(END_Y, START_Y, GRANULARITY)
+Z_RANGE = worldToIndex(END_Z, START_Z, GRANULARITY)
+
+print(X_RANGE, Y_RANGE, Z_RANGE)
+
+# create a numpy 3D array of False values
+collision_map = np.zeros((X_RANGE, Y_RANGE, Z_RANGE), dtype=bool)
+print(collision_map.shape)
+
+# iterate over every vertex in every mesh in world coordinates
+# find the corresponding index in the collision_map and set it to True
+for mesh in COLLISION_MESHES:
+    bm = bmesh.new()
+    bm.from_mesh(mesh.data)
+    bm.transform(mesh.matrix_world)
+    for v in bm.verts:
+        x = worldToIndex(v.co.x, START_X, GRANULARITY)
+        y = worldToIndex(v.co.y, START_Y, GRANULARITY)
+        z = worldToIndex(v.co.z, START_Z, GRANULARITY)
+        if 0 <= x < X_RANGE and 0 <= y < Y_RANGE and 0 <= z < Z_RANGE:
+            collision_map[x, y, z] = True
+        else:
+            print("out of range")
+    bm.free()
+
+# print the number of occupied cells
+print(np.sum(collision_map))
+# print the average index of occupied cells transformed back to world coordinates. This is the center of mass of the occupied cells
+print(np.mean(np.argwhere(collision_map), axis=0) / GRANULARITY + [START_X, START_Y, START_Z])
+
+print(collision_map)
+
+# save the collision map to a file
+np.save("collision_map.npy", collision_map)
---
+++ validation/utils/createSDF.py
@@ -0,0 +1,28 @@
+# create a signed distance field from the collision map
+
+import numpy as np
+import scipy.ndimage
+import matplotlib.pyplot as plt
+
+# import the collision map from collision_map.npy
+collision_map = np.load("collision_map.npy")
+
+# invert the collision map
+collision_map = ~collision_map
+
+# create a signed distance field from the collision map
+sdf = scipy.ndimage.distance_transform_edt(collision_map)
+
+# plot the signed distance field in 3D where the distance is color coded
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# ax.voxels(sdf < 0.1, edgecolor='k')
+# ax.set_aspect('equal')
+
+# plt.show()
+
+# multiply the signed distance field by the resolution to get the distance in meters
+sdf /= 20 # 20 is the resolution of the collision map. This is the same as the GRANULARITY in createCollisionMap.py
+
+# save the signed distance field to a file
+np.save("sdf.npy", sdf)
