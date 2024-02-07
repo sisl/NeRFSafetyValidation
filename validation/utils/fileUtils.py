4e2ea55922a19f1bc645245f1b571a3821380b7d
Jacob Frausto <jacob_frausto@brown.edu>
2024-02-06 21:32:01 -0800

    Update fileUtils.py

--- validation/utils/fileUtils.py
+++ validation/utils/fileUtils.py
@@ -5,12 +5,9 @@ def cache_poses(pose_file_path, cost_file_path, destination_dir):
     # Ensure the destination directory exists
     os.makedirs(destination_dir, exist_ok=True)
     
-    # Create the 'cached' directory within the destination directory
-    cached_dir = os.path.join(destination_dir, 'cached')
-    
     # Create individual directories for each file within the 'cached' directory
-    pose_dir = os.path.join(cached_dir, 'poses')
-    cost_dir = os.path.join(cached_dir, 'costs')
+    pose_dir = os.path.join(destination_dir, 'poses')
+    cost_dir = os.path.join(destination_dir, 'costs')
     os.makedirs(pose_dir, exist_ok=True)
     os.makedirs(cost_dir, exist_ok=True)
 
@@ -19,9 +16,9 @@ def cache_poses(pose_file_path, cost_file_path, destination_dir):
     
     # Copy the files
     for file in pose_files:
-        shutil.copy(pose_file_path, file)
+        shutil.copy(os.path.join(pose_file_path, file), pose_dir)
     for file in cost_files:
-        shutil.copy(cost_file_path, file)
+        shutil.copy(os.path.join(cost_file_path, file), cost_dir)
     print("Caching posts & costs!")
 
 def restore_poses(cached_pose_dir, cached_cost_dir, destination_dir):
