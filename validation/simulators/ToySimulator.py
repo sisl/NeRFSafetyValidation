ea184a14ad050dd072c28dba4ee84b9ed7823ebb
Yash Kadadi <yamu1001@gmail.com>
2024-02-20 01:43:07 +0000

    added plotting to toy problem

--- validation/simulators/ToySimulator.py
+++ validation/simulators/ToySimulator.py
@@ -17,13 +17,13 @@ class ToySimulator:
 
     def step(self, noise):
         self.position += noise
-        collision_value = np.linalg.norm(self.position)
-        is_collision = collision_value > self.collision_threshold
+        collision_value = -np.linalg.norm(self.position - torch.tensor([5.0, 5.0]))
+        is_collision = np.linalg.norm(self.position) > self.collision_threshold
         return is_collision, collision_value, self.position
 
-f = lambda current_position, goal_position: -np.linalg.norm(current_position - goal_position)
-q = [MultivariateNormal(torch.zeros(2), torch.eye(2)) for _ in range(12)]
-p = [MultivariateNormal(torch.zeros(2), torch.eye(2)) for _ in range(12)]
+# f = lambda current_position, goal_position: -np.linalg.norm(current_position - goal_position)
+q = [MultivariateNormal(torch.zeros(2), torch.eye(2)*0.25) for _ in range(12)]
+p = [MultivariateNormal(torch.zeros(2), torch.eye(2)*0.25) for _ in range(12)]
 collision_threshold = 10.0
 goal_position = np.array([5.0, 5.0])
 
@@ -36,6 +36,6 @@ noise_std = torch.ones(12, 2)
 # noise_meanAll = []
 
 simulator = ToySimulator(collision_threshold)
-cem = CrossEntropyMethod(simulator, f, q, p, 4, 3, 3, noise_mean, noise_std, None, None)
+cem = CrossEntropyMethod(simulator, q, p, 10, 3, 50, None, None)
 
 means, covs, q, best_solutionMean, best_solutionCov, best_objective_value = cem.optimize()
--- validation/stresstests/CrossEntropyMethod.py
+++ validation/stresstests/CrossEntropyMethod.py
@@ -5,15 +5,17 @@ import numpy as np
 from scipy.stats import norm
 from validation.utils.blenderUtils import runBlenderOnFailure
 
+import matplotlib.pyplot as plt
+
 import pdb
 
 class CrossEntropyMethod:
-    def __init__(self, simulator, f, q, p, m, m_elite, kmax, noise_mean, noise_std, blend_file, workspace):
+    def __init__(self, simulator, q, p, m, m_elite, kmax, blend_file, workspace):
         """
         Initialize the CrossEntropyMethod class.
 
         Args:
-            f: function to maximize
+            f: function to maximize. The simulator itself will return the value of the maximization function, so we don't need to pass it in.
             q: proposal distribution
             p: target distribution
             m: number of samples per iteration
@@ -21,7 +23,6 @@ class CrossEntropyMethod:
             kmax: number of iterations
         """
         self.simulator = simulator
-        self.f = f # 10 x 1 (one score for each simulation)
         self.q = q # 12 x 12 (12 step #s and 12 noise parameters)
         self.p = p # same as above
         self.m = m # 13?
@@ -31,10 +32,10 @@ class CrossEntropyMethod:
         self.covs = [0] * 12
         self.collisions = 0
         self.stepsToCollision = 0
-        self.noise_mean = noise_mean.cpu().numpy()
-        self.noise_std = noise_std.cpu().numpy()
         self.blend_file = blend_file
         self.workspace = workspace
+
+        self.TOY_PROBLEM = True
 
     def trajectoryLikelihood(self, noise):
         # get the likelihood of a noise measurement by finding each element's probability, logging each, and returning the sum
@@ -53,56 +54,80 @@ class CrossEntropyMethod:
             best_solution: best solution found during the optimization process
             best_objective_value: value of the function_to_maximize at the best_solution
         """
+
+        populationScores = []
+        eliteScores = []
+        zeroedWeight = False # still unsure how this happens, but takes care of when the weights are all 0
+
         for k in range(self.kmax):
             print(f"Starting population {k}")
             # sample and evaluate function on samples
-            # samples = self.q.sample((self.m,))
             population = [] # 10 x 12 x 12 array (one noise for every simulation)
             risks = np.array([])
             outputSimulationList = []
 
-            for simulationNumber in range(10):
+
+            if self.TOY_PROBLEM:
+                # plot the path of each simulation
+                plt.figure()
+            
+            for simulationNumber in range(self.m):
                 # ONE SIMULATION BELOW
                 self.simulator.reset()
-                trajectory = []
+                noises = [self.q[stepNumber].sample() for stepNumber in range(12)] # precompute noise values
+                trajectory = [noise.cpu().numpy() for noise in noises]
+
+                if self.TOY_PROBLEM:
+                    positions = np.array([[0, 0]], dtype=float)
+
                 step = []
-                riskSteps = []
+                riskSteps = np.array([])
                 outputStepList = []
                 everCollided = False
 
                 for stepNumber in range(12):  
-                    noise = self.q[stepNumber].sample()
-                    isCollision, collisionVal, currentPos = self.simulator.step(noise)
+                    # noise = self.q[stepNumber].sample()
+                    isCollision, collisionVal, currentPos = self.simulator.step(noises[stepNumber])
+
+
+                    if self.TOY_PROBLEM:
+                        # append the current position to positions
+                        positions = np.append(positions, [currentPos], axis=0)
 
                     # append the noises
-                    noiseList = noise.cpu().numpy()
-                    outputStepList.extend(noiseList)
-                    step = noiseList
+                    # noiseList = noise.cpu().numpy()
+                    outputStepList.extend(trajectory[stepNumber])
+                    # step = noiseList
 
                     # append the sdf value and positions
                     outputStepList.append(collisionVal)
                     outputStepList.extend(currentPos)
 
                     # store sdf value
-                    riskSteps.append(collisionVal)
-                    trajectory.append(step) # store noise in trajectory
+                    riskSteps = np.append(riskSteps, collisionVal)
+                    # trajectory.append(step) # store noise in trajectory
 
                     # check for collisions
                     if isCollision:
                         self.collisions += 1
                         self.stepsToCollision += stepNumber
                         everCollided = True
-                        runBlenderOnFailure(self.blend_file, self.workspace, simulationNumber, stepNumber)
+                        # runBlenderOnFailure(self.blend_file, self.workspace, simulationNumber, stepNumber)
                         break
+                
+                if self.TOY_PROBLEM:
+                    # plot the path of the simulation
+                    plt.plot(positions[:, 0], positions[:, 1])                
 
                 # store trajectory (for a simulation)
                 population.append(trajectory)
-                risks = np.append(risks, min(riskSteps)) # store the smallest sdf value
-
-                # calculate likelihood of the trajectory
-                likelihood = self.trajectoryLikelihood(noiseList) # TODO: check this
-                outputStepList.append(likelihood)
+                if self.TOY_PROBLEM:
+                    risks = np.append(risks, riskSteps[-1]) # store the distance to the goal at the last step
+                else:
+                    risks = np.append(risks, min(riskSteps)) # store the smallest sdf value (the closest we get to a crash)
                 
+                
+
                 # output the collision value
                 outputStepList.append(isCollision)
 
@@ -110,17 +135,30 @@ class CrossEntropyMethod:
                 outputSimulationList.append(outputStepList)
 
                 # write results to CSV using the format in MonteCarlo.py
-                with open("./results/collisionValues.csv", "a") as csvFile:
-                    print(f"Noise List: {noiseList}")
-                    writer = csv.writer(csvFile)
-                    for outputStepList in outputSimulationList:
-                        outputStepList.append(everCollided)
-                        writer.writerow(outputStepList) 
+                # with open("./results/collisionValues.csv", "a") as csvFile:
+                #     print(f"Noise List: {noiseList}")
+                #     writer = csv.writer(csvFile)
+                #     for outputStepList in outputSimulationList:
+                #         outputStepList.append(everCollided)
+                #         writer.writerow(outputStepList) 
 
+            if self.TOY_PROBLEM:
+                # plot a star at the start and goal positions
+                plt.plot(0, 0, 'ro')
+                plt.plot(5, 5, 'go')
+                plt.title(f"Simulation Paths for Population {k}")
+                plt.savefig(f"./results/pltpaths/simulationPaths_pop{k}.png")
+                plt.close()
+
+            print(f"Average score of population {k}: {risks.mean()}")
+            populationScores.append(risks.mean())
             # select elite samples and compute weights
             elite_indices = np.argsort(risks)[-self.m_elite:]
             elite_samples = torch.tensor(np.array(population)[elite_indices])
-
+            # print average score of elite samples
+            print(f"Average score of elite samples from population {k}: {risks[elite_indices].mean()}")
+            eliteScores.append(risks[elite_indices].mean())
+            
             # print the elite samples and their indices
             # print(f"Elite Samples: {elite_samples}")
             # print(f"Elite Indices: {elite_indices}")
@@ -135,7 +173,7 @@ class CrossEntropyMethod:
                 
                 # normalize the weights
                 weights[i] = weights[i] / weights[i].sum()
-                # pdb.set_trace()
+                
 
                 # update proposal distribution based on elite samples
                 # mean = (elite_samples[:, i] * weights[i]).sum()
@@ -145,16 +183,42 @@ class CrossEntropyMethod:
                     diff = elite_samples[j, i] - mean
                     cov += weights[i][j] * torch.outer(diff, diff)
                 cov = cov + 1e-1 * torch.eye(self.q[i].event_shape[0])  # add a small value to the diagonal for numerical stability
-                # pdb.set_trace()
 
                 self.means[i] = mean
                 self.covs[i] = cov
-                self.q[i] = MultivariateNormal(mean, cov)
+                try:
+                    self.q[i] = MultivariateNormal(mean, cov)
+                except ValueError:
+                    # may occur if f is improperly specified
+                    # pdb.set_trace()
+                    print(mean, cov)
+                    print(f"Highly improbable weights at step {i} in population {k}! Exiting...")
+
+                    zeroedWeight = True
+                    break
+            
+            if zeroedWeight:
+                break
 
             # print the updated proposal distribution
-            print(f"Updated Proposal Distribution:")
-            for i in range(12):
-                print(f"Step {i}: Mean: {self.means[i]}, Covariance: {self.covs[i]}")
+            # print(f"Updated Proposal Distribution:")
+            # for i in range(12):
+            #     print(f"Step {i}: Mean: {self.means[i]}, Covariance: {self.covs[i]}")
+
+
+        # plot the population and elite scores
+
+        plt.figure()
+        plt.plot(populationScores)
+        plt.plot(eliteScores)
+        plt.legend(["Population", "Elite"])
+
+        # x and y labels
+        plt.xlabel("Population #")
+        plt.ylabel("Average Score")
+        plt.savefig(f"./results/pltpaths/populationScores.png")
+        plt.close()
+
 
 
         print("===FINISHED OPTIMIZATION===")
