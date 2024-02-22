c471bb17b97c0bf8e85c4b98bd47dd6735c0a4bc
Jacob Frausto <jacob_frausto@brown.edu>
2024-02-21 19:08:47 -0800

    fix small tensor bug

--- validation/utils/mathUtils.py
+++ validation/utils/mathUtils.py
@@ -1,8 +1,9 @@
 import numpy as np
 
 def is_positive_definite(matrix):
+    matrix_copy = matrix.cpu().numpy()
     try:
-        np.linalg.cholesky(matrix)
+        np.linalg.cholesky(matrix_copy)
         return True
     except np.linalg.LinAlgError:
         return False
