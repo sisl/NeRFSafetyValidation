07e14826212c1ee3e7aa36df821c9dd4c2a09a80
Jacob Frausto <jacob_frausto@brown.edu>
2024-04-10 12:40:27 -0700

    extend NumpyEncoder to include bools

--- validation/utils/NumpyEncoder.py
+++ validation/utils/NumpyEncoder.py
@@ -7,4 +7,6 @@ class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.float32):
             return float(obj)
+        elif isinstance(obj, np.bool_):
+            return bool(obj)
         return json.JSONEncoder.default(self, obj)
