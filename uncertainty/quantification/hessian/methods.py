5d006f29b02766c52a6cb3edeaf272d03eab44fe
Jacob Frausto <jacob_frausto@brown.edu>
2024-04-29 12:37:51 -0700

    Update methods.py

--- uncertainty/quantification/hessian/methods.py
+++ uncertainty/quantification/hessian/methods.py
@@ -18,7 +18,6 @@ def finite_difference(x, func, epsilon):
         size = x.size()
         hessian = torch.zeros(n, n)
         f_x = func(x)
-        pdb.set_trace()
         grad_x = torch.autograd.grad(f_x, x, create_graph=True, allow_unused=True)[0]
 
         for i in range(n):
@@ -44,8 +43,8 @@ def bfgs(x, func):
     """
     x_tensor = torch.tensor(x, requires_grad=True)
     x_numpy = x_tensor.detach().numpy()
-    func_value = lambda x: func(torch.tensor(x, requires_grad=True)).detach().item()
-    grad = lambda x: torch.autograd.grad(func(torch.tensor(x, requires_grad=True)), x_tensor, retain_graph=True)[0].detach().numpy()
+    func_value = lambda x: func(torch.tensor(x, requires_grad=True))
+    grad = lambda x: torch.autograd.grad(func_value(x_tensor), x_tensor, retain_graph=True, allow_unused=True)[0].detach().numpy()
     hessian_inv = fmin_bfgs(func_value, x_numpy, fprime=grad, disp=False, full_output=True)[3]
     hessian = torch.inverse(torch.tensor(hessian_inv, dtype=torch.float))
     return hessian
