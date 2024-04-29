e896372b5d13619e94020a3453078facd2e138d9
Jacob Frausto <jacob_frausto@brown.edu>
2024-04-29 12:40:22 -0700

    Update methods.py

--- uncertainty/quantification/hessian/methods.py
+++ uncertainty/quantification/hessian/methods.py
@@ -43,8 +43,9 @@ def bfgs(x, func):
     """
     x_tensor = torch.tensor(x, requires_grad=True)
     x_numpy = x_tensor.detach().numpy()
-    func_value = lambda x: func(torch.tensor(x, requires_grad=True))
-    grad = lambda x: torch.autograd.grad(func_value(x_tensor), x_tensor, retain_graph=True, allow_unused=True)[0].detach().numpy()
+    func_value = lambda x: func(torch.tensor(x, requires_grad=True)).detach().item()
+    grad = lambda x: torch.autograd.grad(func(torch.tensor(x, requires_grad=True)), x_tensor, retain_graph=True, allow_unused=True)[0].detach().numpy()
+    pdb.set_trace()
     hessian_inv = fmin_bfgs(func_value, x_numpy, fprime=grad, disp=False, full_output=True)[3]
     hessian = torch.inverse(torch.tensor(hessian_inv, dtype=torch.float))
     return hessian
