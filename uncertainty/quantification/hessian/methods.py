99c2b8da6a1c1ebe0e0dc86c089be7161db92de9
Jacob Frausto <jacob_frausto@brown.edu>
2024-04-29 12:59:02 -0700

    Update methods.py

--- uncertainty/quantification/hessian/methods.py
+++ uncertainty/quantification/hessian/methods.py
@@ -1,7 +1,6 @@
 import numpy as np
 import torch
-from scipy.optimize import fmin_bfgs
-import numpy.linalg as lin
+from torch.optim import LBFGS
 import pdb
 
 
@@ -34,37 +33,9 @@ def finite_difference(x, func, epsilon):
 
         return hessian.view(*size, *size)
 
-
-def linesearch_secant(f, d, x):
-    epsilon=10**(-5)
-    max = 500
-    alpha_curr=0
-    alpha=10**-5
-    y,grad=f(x)
-    dphi_zero=np.dot(np.array(grad).T,d)
-
-    dphi_curr=dphi_zero
-    i=0
-
-    while np.abs(dphi_curr)>epsilon*np.abs(dphi_zero):
-        alpha_old=alpha_curr
-        alpha_curr=alpha
-        dphi_old=dphi_curr
-        y,grad=f(x+alpha_curr*d)
-        dphi_curr=np.dot(np.array(grad).T,d)
-        alpha=(dphi_curr*alpha_old-dphi_old*alpha_curr)/(dphi_curr-dphi_old)
-        i += 1
-        if (i >= max) and (np.abs(dphi_curr)>epsilon*np.abs(dphi_zero)):
-            print('Line search terminating with number of iterations:')
-            print(i)
-            print(alpha)
-            break
-        
-    return alpha
-
 def bfgs(x, func):
     """
-    Approximate the Hessian using the BFGS method (scipy optimizer).
+    Approximate the Hessian using the LBFGS method.
 
     Parameters:
     x: the point at which to compute the Hessian.
@@ -74,33 +45,26 @@ def bfgs(x, func):
     Returns:
     hessian: the approximated Hessian matrix.
     """
-    hessian = np.eye(len(x))
-    tol = 1e-20
-    y,grad = func(x)
-    dist=2*tol
-    epsilon = tol
-    iter=0
+    # optimizer
+    optimizer = LBFGS([x], lr=1)
 
-    while lin.norm(grad)>1e-6:
-        value,grad=func(x)
-        print (grad)
-        p=np.dot(-hessian,grad)
-        lam = linesearch_secant(func,p,x)
-        iter += 1
-        xt = x
-        x = x + lam*p
-        s = lam*p
-        dist=lin.norm(s)
-        newvalue,newgrad=func(x)
-        y = np.array(newgrad)-grad
-        rho=1/np.dot(y.T,s)
-        s = s.reshape(2,1)
-        y = y.reshape(2,1)
-        tmp1 = np.eye(2)-rho*np.dot(s,y.T)
-        tmp2 = np.eye(2)-rho*np.dot(y,s.T)
-        tmp3 = rho*np.dot(s,s.T)
-        hessian= np.dot(np.dot(tmp1,hessian),tmp2) + tmp3
-        
+    def closure():
+        optimizer.zero_grad()
+        output = func(x)
+        output.backward(create_graph=True)
+        return output
+
+    optimizer.step(closure)
+
+
+    grad = x.grad
+    hessian = torch.zeros(len(x), len(x))
+
+    for i in range(len(grad)):
+        grad[i].backward(retain_graph=True)
+        hessian[i] = x.grad
+        x.grad.zero_()
+
     return hessian
 
 def regression_gradient(theta, func, perturbations=100, delta=0.01):
