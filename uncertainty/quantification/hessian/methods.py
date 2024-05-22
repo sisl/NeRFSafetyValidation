b71fa7bd5bd24d23f5da9193e49a227e92c7787f
Jacob Frausto <jacob_frausto@brown.edu>
2024-05-21 18:00:57 -0700

    Update methods.py

--- uncertainty/quantification/hessian/methods.py
+++ uncertainty/quantification/hessian/methods.py
@@ -24,6 +24,7 @@ def finite_difference(x, func, epsilon):
         f_x = func(xt)
         if isinstance(f_x, float):
             f_x = torch.tensor([f_x], dtype=torch.float32)
+            f_x.requires_grad_(True)
         grad_x = torch.autograd.grad(f_x, xt, create_graph=True, allow_unused=True)[0]
 
         for i in range(n):
@@ -33,8 +34,9 @@ def finite_difference(x, func, epsilon):
                 x_i_plus_epsilon = x_i.clone()  # create a new tensor to avoid in-place operation
                 x_i_plus_epsilon[i] = x_i[i] + epsilon
                 f_x_i = func(x_i_plus_epsilon)
-                if isinstance(f_x_epsilon, float):
-                    f_x_epsilon = torch.tensor([f_x_epsilon], dtype=torch.float32)
+                if isinstance(f_x_i, float):
+                    f_x_i = torch.tensor([f_x_i], dtype=torch.float32)
+                    f_x_i.requires_grad_(True)
                 grad_x_i = torch.autograd.grad(f_x_i, x_i_plus_epsilon, create_graph=False, allow_unused=True)[0]
                 hessian[i, j] = (grad_x_i[i] - grad_x) / epsilon
 
