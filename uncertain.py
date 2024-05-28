1f35ad2573a7969bdb0eaf7b0652efd5847a2e40
Jacob Frausto <jacob_frausto@brown.edu>
2024-05-27 20:36:28 -0700

    numeric stability

--- uncertain.py
+++ uncertain.py
@@ -134,6 +134,10 @@ def uncertainty(method, path_to_images=None, rendered_output=None, model_to_use=
                 pos_mu = bayesian_laplace.get_posterior_mean()
                 pos_cov = bayesian_laplace.get_posterior_cov()
                 n = pos_cov.shape[0]
+                
+                # ensure numeric stability
+                diag_indices = np.diag_indices(n)
+                pos_cov[diag_indices] = np.maximum(0, pos_cov[diag_indices])
 
                 # Trace of the covariance matrix
                 trace = np.trace(pos_cov) / n
